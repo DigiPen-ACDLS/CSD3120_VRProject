e8d7d9faff146c20d726a11e2f20eb64400606d2
Diren D Bharwani <diren.bharwani@gmail.com>
2023-03-24 04:12:01 +0800

    Fixed tsconfg.json intialisation in batch file
    Harcoded cus idk how to override the outputDir, sue me

--- install.bat
+++ install.bat
@@ -14,43 +14,37 @@ call npm --v && (
 
 cd SolarSystemVR
 
-
-set state=%errorlevel%
 :: Initialise the npm with defaults
 if not exist package.json (
   echo Initialising NPM project...
   call npm init -y
-  set state=Installing
   goto install
 ) else (
-  echo NPM Project Initialised! Updating Dependencies...
-  set state=Updating
-  goto update
+  echo Updating Dependencies...
+  call npm update
+  echo All Dependencies Updated!
+  goto ok
 )
 
 : install 
 :: Install dependencies for this project
 :: Webpack
 
-echo %state% Webpack...
+echo Installing Webpack...
 call npm install --save-dev webpack@latest webpack-cli@latest webpack-dev-server@latest
 call npm install --save-dev html-webpack-plugin@latest copy-webpack-plugin@latest
 echo ----------------
 echo Webpack Installed!
 echo.
 
-echo %state% Typescript...
+echo Installing Typescript...
 call npm install --save-dev typescript@latest ts-loader@latest
 
-if not exist tsconfig.json (  
-  :: Create and initialise tsconfig
-  call npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
-)
 echo ----------------
 echo Typescript Installed!
 echo.
 
-echo %state% BabylonJS...
+echo Installing BabylonJS...
 call npm install --save babylonjs@latest babylonjs-gui@latest babylonjs-loaders@latest babylonjs-materials@latest
 echo ----------------
 echo BabylonJS Installed!
@@ -59,14 +53,44 @@ echo.
 echo All Dependencies Installed!
 goto ok
 
-:update
-
-echo %state% Dependencies...
-call npm update
-
-echo All Dependencies Updated!
-goto ok
-
-
 :ok
+
+:: Check if tsconfig exists
+if not exist tsconfig.json (
+
+  :: Create and initialise tsconfig.json with the following
+  (
+    echo {
+    echo "compilerOptions": {
+    echo  /* Language and Environment */
+    echo  "target": "es2016",                         /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations. */
+    echo  "lib": ["es6"],                             /* Specify a set of bundled library declaration files that describe the target runtime environment. */
+    echo.  
+    echo  /* Modules */
+    echo  "module": "commonjs",                       /* Specify what module code is generated. */
+    echo  "rootDir": "src",                           /* Specify the root folder within your source files. */
+    echo  "resolveJsonModule": true,                  /* Enable importing .json files. */
+    echo.
+    echo  /* JavaScript Support */
+    echo  "allowJs": true,                            /* Allow JavaScript files to be a part of your program. Use the 'checkJS' option to get errors from these files. */
+    echo.
+    echo  /* Emit */
+    echo  "outDir": "dist",                           /* Specify an output folder for all emitted files. */
+    echo.
+    echo  /* Interop Constraints */
+    echo  "esModuleInterop": true,                    /* Emit additional JavaScript to ease support for importing CommonJS modules. This enables 'allowSyntheticDefaultImports' for type compatibility. */
+    echo  "forceConsistentCasingInFileNames": true,   /* Ensure that casing is correct in imports. */
+    echo.
+    echo  /* Type Checking */
+    echo  "strict": true,                             /* Enable all strict type-checking options. */
+    echo  "noImplicitAny": true,                      /* Enable error reporting for expressions and declarations with an implied 'any' type. */
+    echo  
+    echo  /* Completeness */
+    echo  "skipLibCheck": true                        /* Skip type checking all .d.ts files. */
+    echo  }
+    echo }
+  ) > tsconfig.json
+)
+
+echo DONE!
 pause
