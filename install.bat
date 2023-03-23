83f124defba00bb5a78de13540f07610fb86ac8d
Diren D Bharwani <diren.bharwani@gmail.com>
2023-03-24 03:46:44 +0800

    Batch script installs or updates all dependencies

--- install.bat
+++ install.bat
@@ -14,31 +14,59 @@ call npm --v && (
 
 cd SolarSystemVR
 
+
+set state=%errorlevel%
 :: Initialise the npm with defaults
 if not exist package.json (
   echo Initialising NPM project...
   call npm init -y
+  set state=Installing
+  goto install
+) else (
+  echo NPM Project Initialised! Updating Dependencies...
+  set state=Updating
+  goto update
 )
 
+: install 
 :: Install dependencies for this project
 :: Webpack
 
-echo Installing Webpack...
-call npm install --save-dev webpack webpack-cli webpack-dev-server
-call npm install --save-dev html-webpack-plugin copy-webpack-plugin
+echo %state% Webpack...
+call npm install --save-dev webpack@latest webpack-cli@latest webpack-dev-server@latest
+call npm install --save-dev html-webpack-plugin@latest copy-webpack-plugin@latest
 echo ----------------
 echo Webpack Installed!
 echo.
 
-echo Installing Typescript...
-call npm install --save-dev typescript ts-loader
+echo %state% Typescript...
+call npm install --save-dev typescript@latest ts-loader@latest
 
-if not exist tsconfig.json (
-  :: Create an empty tsconfig file and initialise it
-  echo. > tsconfig.json
+if not exist tsconfig.json (  
+  :: Create and initialise tsconfig
+  call npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
 )
 echo ----------------
 echo Typescript Installed!
 echo.
 
+echo %state% BabylonJS...
+call npm install --save babylonjs@latest babylonjs-gui@latest babylonjs-loaders@latest babylonjs-materials@latest
+echo ----------------
+echo BabylonJS Installed!
+echo.
+
+echo All Dependencies Installed!
+goto ok
+
+:update
+
+echo %state% Dependencies...
+call npm update
+
+echo All Dependencies Updated!
+goto ok
+
+
+:ok
 pause
