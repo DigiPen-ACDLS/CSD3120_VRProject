c105b4c81c2bbf0f56e46ac77eaae1adce2eda29
Diren D Bharwani <diren.bharwani@gmail.com>
2023-03-25 17:07:32 +0800

    Reverted structure. Batch file now runs and updates dependecies

---
+++ SolarSystemVR/webpack.config.js
@@ -0,0 +1,42 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path              = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = 
+{
+    mode        : "development",
+    target      : "web",
+    entry       : {
+      index     : './src/index.ts',
+    },
+    devtool     : 'inline-source-map',
+    devServer   : {
+      static    : true,
+      port      : 3000,
+      open      : true,
+      hot       : true,
+      liveReload: true
+    },
+    plugins     : [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+      }),
+    ],
+    resolve     : {
+        extensions: [
+          ".ts", ".js"
+        ]
+    },
+    module      : {
+        rules     : [{ 
+          test  : /\.tsx?$/, 
+          loader: "ts-loader" 
+        }]
+    },
+    output      : {
+      filename  : 'index.js',
+      path      : path.resolve(__dirname, 'dist'),
+    },
+};
--- run.bat
+++ run.bat
@@ -9,94 +9,25 @@ echo NPM Version:
 call npm --v && (
   echo --------------------------------
 ) || echo NPM is not installed! && (
-  call npm install -g npm
+  call npm install
 )
 
 if not exist SolarSystemVR\ ( mkdir SolarSystemVR )
 cd SolarSystemVR
 
 :: Initialise the npm with defaults
-if not exist package.json (
-  echo Initialising NPM project...
-  call npm init -y
-  goto install
-) else (
+choice /c YN /m "Update Dependencies"
+set e=%errorlevel%
 
-  echo Project has already been created!
-  choice /c YN /m "Update Dependencies"
-  set e=%errorlevel%
-  
-  if e==Y (goto update) else (goto verify)
-)
+if e==Y (goto update) else (goto run)
 
 :update
 echo Updating Dependencies...
 call npm update
 echo All Dependencies Updated!
-goto verify
+goto run
 
-
-
-:install 
-:: Install dependencies for this project
-:: Webpack
-
-echo --------------------------------
-echo Installing Webpack..
-echo --------------------------------
-call npm install --save-dev webpack@latest webpack-cli@latest webpack-dev-server@latest
-call npm install --save-dev html-webpack-plugin@latest copy-webpack-plugin@latest
-echo --------------------------------
-echo Webpack Installed!
-echo.
-
-echo --------------------------------
-echo Installing Typescript...
-echo --------------------------------
-call npm install --save-dev typescript@latest ts-loader@latest
-echo --------------------------------
-echo Typescript Installed!
-echo.
-
-echo --------------------------------
-echo Installing BabylonJS...
-echo --------------------------------
-call npm install --save babylonjs@latest babylonjs-gui@latest babylonjs-loaders@latest babylonjs-materials@latest
-echo --------------------------------
-echo BabylonJS Installed!
-echo.
-
-echo All Dependencies Installed!
-goto verify
-
-:verify
-
-echo Verifying files...
-echo.
-
-:: Copy config files
-call xcopy /s /y ..\Config\ .\ 
-
-if not exist src\ (
-  
-  echo --------------------------------
-  echo Creating src directory...
-  echo --------------------------------
-  
-  mkdir src
-)
-
-::Copy files from Source to src
-call xcopy /s /y ..\Source src\
-
-echo --------------------------------
-echo Files Verified!
-echo.
-
-goto ok
-
-:ok
-echo DONE!
+:run
 echo Running SolarSystemVR...
 call npm run start
 
